f36fc2763ff65428abf9b52af383c81392e7a233
siddhisuthar <siddhi@siddhis-macbook-pro.local>
2017-05-23 20:39:16 -0700

    follow users update

--- SocialNetworkApp/SecondViewController.swift
+++ SocialNetworkApp/SecondViewController.swift
@@ -7,17 +7,58 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UISearchResultsUpdating {
 
+    @IBOutlet weak var followUsersTableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    var usersArray = [NSDictionary?]()
+    var filteredUsers = [NSDictionary?]()
+    var databaseRef = FIRDatabase.database().reference()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        searchController.searchResultsUpdater = self as! UISearchResultsUpdating
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        followUsersTableView.tableHeaderView = searchController.searchBar
+        
+        databaseRef.child("user_profiles").queryOrdered(byChild: "name").observe(.childAdded, with: {
+        (snapshot) in
+        
+            self.usersArray.append(snapshot.value as? NSDictionary)
+            //insert rows
+            self.followUsersTableView.insertRows(at: [IndexPath(row: self.usersArray.count-1, section: 0)], with: UITableViewRowAnimation.automatic)
+            
+        })
+        { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//     
+//        return cell
+//    }
+    func updateSearchResults(for searchController: UISearchController) {
+        
     }
 
 
